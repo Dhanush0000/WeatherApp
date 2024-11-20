eda7998b134da1d558be1e56c5944e9cf8f06b30
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-20 08:00:56 +0530

    Defining WeatherApp class

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -2,8 +2,12 @@ import javax.swing.*;
 
 public class WeatherApp extends JFrame{
     public WeatherApp(){
-        super(title:"Weather App");
-
+        super("Weather App");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(450, 650);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
     }
 
 }
