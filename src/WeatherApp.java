7843704e0614242f8ebed147d1d90ec8ebf82eae
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-20 08:13:34 +0530

    Adding search image icon for search button

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,5 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class WeatherApp extends JFrame{
     public WeatherApp(){
@@ -19,6 +23,23 @@ public class WeatherApp extends JFrame{
         searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
 
         add(searchTextField);
+
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+    }
+    private ImageIcon loadImage(String resourcePath){
+        try {
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            return new ImageIcon(image);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("Could not find resource");
+        return null;
     }
 
 }
