22a7a9a394e8c60b4e43108248552ba9d6ebc01b
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-21 19:28:17 +0530

    Building API url with location parameter

--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -1,2 +1,23 @@
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
 public class WeatherApp {
+    //fetch weather data
+    public static JSONObject getWeatherData(String locationName){
+        JSONArray locationData = getLocationData(locationName);
+    }
+
+    private static JSONArray getLocationData(String locationName){
+        locationName = locationName.replaceAll(" ", "+");
+
+        //build API url with location parameter
+        String urlString = "https://geocoding-api.com-meteo.com/v1/search?name=" +
+                locationName + "&count=1-&language=en&format=json";
+
+        try {
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
