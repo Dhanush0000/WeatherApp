c9f5c812be29b429ed5b8d56173df8e73992eafd
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-24 08:22:26 +0530

    Solving API call error

--- src/AppLauncher.java
+++ src/AppLauncher.java
@@ -6,7 +6,7 @@ public class AppLauncher {
             @Override
             public void run() {
                 //new WeatherAppGUI().setVisible(true);
-                System.out.println(WeatherApp.getLocationData("Tokyo"));
+                System.out.println(WeatherApp.getLocationData("Berlin"));
             }
         });
     }
