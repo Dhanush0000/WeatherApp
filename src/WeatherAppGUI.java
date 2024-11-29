c1097e9cd00534021af3c4c70bd13c5f25c347d2
g-n-s-r <147964739+g-n-s-r@users.noreply.github.com>
2024-11-29 19:00:00 +0530

    Take input from the users

--- src/WeatherAppGUI.java
+++ src/WeatherAppGUI.java
@@ -1,11 +1,17 @@
+import org.json.simple.JSONObject;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class WeatherAppGUI extends JFrame{
+    private JSONObject weatherData;
+
     public WeatherAppGUI(){
         super("Weather App");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -24,11 +30,6 @@ public class WeatherAppGUI extends JFrame{
 
         add(searchTextField);
 
-        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
-
-        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        searchButton.setBounds(375, 13, 47, 45);
-        add(searchButton);
 
         JLabel weatherConditionImage = new JLabel(loadImage("src/assets/cloudy.png"));
         weatherConditionImage.setBounds(0, 125, 450, 217);
@@ -69,6 +70,26 @@ public class WeatherAppGUI extends JFrame{
         windspeedText.setBounds(310, 500, 85, 55);
         windspeedText.setFont(new Font("Dialog", Font.PLAIN, 16));
         add(windspeedText);
+
+        //search Button
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+
+        searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchButton.setBounds(375, 13, 47, 45);
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //get location from user
+                String userInput = searchTextField.getText();
+
+                if (userInput.replaceAll("\\s", "").length() <= 0){
+                    return;
+                }
+
+                weatherData = WeatherApp.getWeatherData(userInput);
+            }
+        });
+        add(searchButton);
     }
     private ImageIcon loadImage(String resourcePath){
         try {
